9e1ef78a43b9d132960dd84969e919dc039381dd
jagarro3 <javiby026@gmail.com>
2022-10-05 22:24:57 +0200

    fix: imagenes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://127.0.0.1:8000/',
+  apiFormulaOne: {
+    url: 'https://f1-live-motorsport-data.p.rapidapi.com/',
+    xRapidAPIKey: '957f5975a1msh113c4b367b24b57p17f3dcjsn6073a31ec53a',
+    xRapidAPIHost: 'f1-live-motorsport-data.p.rapidapi.com',
+  },
 };
