e95642857892c10a74e7831b6cf8b353a3b5e419
jagarro3 <javiby026@gmail.com>
2022-10-05 23:20:25 +0200

    no message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/',
+  apiUrl: 'https://apiepub.up.railway.app/',
   apiFormulaOne: {
     url: 'https://f1-live-motorsport-data.p.rapidapi.com/',
     xRapidAPIKey: '957f5975a1msh113c4b367b24b57p17f3dcjsn6073a31ec53a',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/',
+  apiUrl: 'https://apiepub.up.railway.app/',
   apiFormulaOne: {
     url: 'https://f1-live-motorsport-data.p.rapidapi.com/',
     xRapidAPIKey: '957f5975a1msh113c4b367b24b57p17f3dcjsn6073a31ec53a',
